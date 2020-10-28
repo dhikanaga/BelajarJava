1120d45ebda8b1288ef30d6b8b4905c25679dcab
dhikanaga <73399787+dhikanaga@users.noreply.github.com>
2020-10-28 18:52:58 +0700

    Tugas pengenalan Java

---
+++ sekolahqa/src/main/java/com/qa/App.java
@@ -0,0 +1,16 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getMyName());
+    }
+    public static String getMyName(){
+        return "Hello my name is Dhika";
+    }
+}
